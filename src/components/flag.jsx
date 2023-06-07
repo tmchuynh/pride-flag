c6a8e8a12fa0c2eaa6e8d6d29bc37a0c734ed252
tmchuynh <tinamchuynh@gmail.com>
2023-06-07 13:06:48 -0700

    generateGradientString

--- src/components/flag.jsx
+++ src/components/flag.jsx
@@ -7,21 +7,35 @@ function PrideFlag({
     numOfColumns,
     staggeredDelay = 100,
     billow = 2,
-  }) {
+}) {
     return (
-      <div className={styles.flag}>
-        {range(numOfColumns).map((columnIndex) => (
-          <div
-            key={columnIndex}
-            className={styles.column}
-            style={{
-              '--billow': columnIndex * billow + 'px',
-              animationDelay: columnIndex * staggeredDelay + 'ms',
-            }}
-          />
-        ))}
-      </div>
+        <div className={styles.flag}>
+            {range(numOfColumns).map((columnIndex) => (
+                <div
+                    key={columnIndex}
+                    className={styles.column}
+                    style={{
+                        '--billow': columnIndex * billow + 'px',
+                        animationDelay: columnIndex * staggeredDelay + 'ms',
+                    }}
+                />
+            ))}
+        </div>
     );
-  }
+}
+
+function generateGradientString(colors) {
+    const numOfColors = colors.length;
+    const segmentHeight = 100 / numOfColors;
+
+    const gradientStops = colors.map((color, index) => {
+        const from = index * segmentHeight;
+        const to = (index + 1) * segmentHeight;
+
+        return `${color} ${from}%, ${color} ${to}%`;
+    });
+
+    return `linear-gradient(to bottom, ${gradientStops.join(', ')})`;
+}
 
 export default PrideFlag;
