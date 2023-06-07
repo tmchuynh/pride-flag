17077c8ecce9b72369428efb7f3e7e945ce29c4c
tmchuynh <tinamchuynh@gmail.com>
2023-06-07 13:06:04 -0700

    update flag.jsx

--- src/components/flag.jsx
+++ src/components/flag.jsx
@@ -4,22 +4,24 @@ import range from 'lodash.range';
 import styles from '../Style.modules/flag.module.css';
 
 function PrideFlag({
-  numOfColumns = 10,
-  staggeredDelay = 100,
-}) {
-  return (
-    <div className={styles.flag}>
-      {range(numOfColumns).map((columnIndex) => (
-        <div
-          key={columnIndex}
-          className={styles.column}
-          style={{
-            animationDelay: columnIndex * staggeredDelay + 'ms',
-          }}
-        />
-      ))}
-    </div>
-  );
-}
+    numOfColumns,
+    staggeredDelay = 100,
+    billow = 2,
+  }) {
+    return (
+      <div className={styles.flag}>
+        {range(numOfColumns).map((columnIndex) => (
+          <div
+            key={columnIndex}
+            className={styles.column}
+            style={{
+              '--billow': columnIndex * billow + 'px',
+              animationDelay: columnIndex * staggeredDelay + 'ms',
+            }}
+          />
+        ))}
+      </div>
+    );
+  }
 
 export default PrideFlag;
